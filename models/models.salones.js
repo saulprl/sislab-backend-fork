e8dd262b5013f53a643b72879e93bbf6f92da184
ruizaul <fimbressaul@hotmail.com>
2023-02-03 14:58:21 -0700

    asd

--- models/models.salones.js
+++ models/models.salones.js
@@ -1,23 +1,28 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const salones = Schema({
-    //TODO: Falta el id no estoy seguro como hacerlo
-    salon1: {
-      type: String,
-      required: [true, 'El salon es obligatorio'],
-    },
-    salon2: {
-        type: String,
-        required: [true, 'El salon es obligatorio'],
-    },
-    salon3: {
-        type: String,
-        required: [true, 'El salon es obligatorio'],
-    },
-    salon4: {
-        type: String,
-        required: [true, 'El salon es obligatorio'],
-    },
-  });
-  
-module.exports = model('Salon', salones);
+const SalonSchema = Schema({
+  salon1: {
+    type: String,
+    required: [true, "El salon es obligatorio"],
+  },
+  salon2: {
+    type: String,
+    required: [true, "El salon es obligatorio"],
+  },
+  salon3: {
+    type: String,
+    required: [true, "El salon es obligatorio"],
+  },
+  salon4: {
+    type: String,
+    required: [true, "El salon es obligatorio"],
+  },
+});
+
+SalonSchema.methods.toJSON = function () {
+  const { __v, _id, ...salon } = this.toObject();
+  salon.uid = _id;
+  return salon;
+};
+
+module.exports = model("Salon", salones);
