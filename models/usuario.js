9547619df12a8b33c6d3c1c22f0b06fdaed7d1f5
ruizaul <fimbressaul@hotmail.com>
2023-02-13 10:14:51 -0700

    timestamps

--- models/usuario.js
+++ models/usuario.js
@@ -1,37 +1,40 @@
 const { Schema, model } = require('mongoose');
 
-const UsuarioSchema = Schema({
-  nombre: {
-    type: String,
-    required: [true, 'El nombre es obligatorio'],
+const UsuarioSchema = Schema(
+  {
+    nombre: {
+      type: String,
+      required: [true, 'El nombre es obligatorio'],
+    },
+    apellidoPaterno: {
+      type: String,
+      required: [true, 'El apellido es obligatorio'],
+    },
+    apellidoMaterno: {
+      type: String,
+      required: [true, 'El apellido es obligatorio'],
+    },
+    correo: {
+      type: String,
+      required: [true, 'El correo es obligatorio'],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'La contraseña  es obligatoria'],
+    },
+    rol: {
+      type: String,
+      required: true,
+      enum: ['ADMIN', 'MAESTRO'],
+    },
+    estado: {
+      type: Boolean,
+      default: true,
+    },
   },
-  apellidoPaterno: {
-    type: String,
-    required: [true, 'El apellido es obligatorio'],
-  },
-  apellidoMaterno: {
-    type: String,
-    required: [true, 'El apellido es obligatorio'],
-  },
-  correo: {
-    type: String,
-    required: [true, 'El correo es obligatorio'],
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: [true, 'La contraseña  es obligatoria'],
-  },
-  rol: {
-    type: String,
-    required: true,
-    enum: ['ADMIN', 'MAESTRO'],
-  },
-  estado: {
-    type: Boolean,
-    default: true,
-  },
-});
+  { timestamps: true }
+);
 
 UsuarioSchema.methods.toJSON = function () {
   const { __v, password, _id, ...usuario } = this.toObject();
