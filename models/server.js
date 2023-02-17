9e2fe5154acd8e1b47148cb46b352c5c341562fb
ruizaul <fimbressaul@hotmail.com>
2023-02-17 04:18:24 -0700

    ss

--- models/server.js
+++ models/server.js
@@ -7,10 +7,6 @@ class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT;
-    this.corsOrigin = corsOrigin = {
-      origin: "http://localhost:3000",
-      optionSuccessStatus: 200,
-    };
 
     this.paths = {
       auth: "/api/auth",
@@ -35,7 +31,7 @@ class Server {
 
   middlewares() {
     // CORS
-    this.app.use(cors(corsOrigin));
+    this.app.use(cors());
 
     // Lectura y parseo del body
     this.app.use(express.json());
