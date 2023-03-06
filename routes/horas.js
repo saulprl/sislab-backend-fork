8df98455ccea1f9df9040e076327ff8b7876ec5a
Anastacio <a219223893@unison.mx>
2023-03-05 19:53:27 -0700

    simplifique las rutas falta agregar los controladores

--- routes/horas.js
+++ routes/horas.js
@@ -1,31 +1,7 @@
 const { Router } = require('express');
-const { check } = require('express-validator');
-
-const {
-  validarCampos,
-} = require('../middlewares');
-
-const {
-  existeUsuarioPorId,
-} = require('../helpers/db-validators');
-
-const {
-  usuarioGet,
-  usuariosGet,
-} = require('../controllers/usuarios');
-
+const horariosGet = require('../controllers/horarios');
 const router = Router();
 
-router.get(
-  '/:id',
-  [
-    check('id', 'No es un ID válido').isMongoId(),
-    check('id').custom(existeUsuarioPorId),
-    validarCampos,
-  ],
-  usuarioGet
-);
-
-router.get('/', usuariosGet);
+router.get('/', horariosGet);
 
 module.exports = router;
