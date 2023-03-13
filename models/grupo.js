ccf7940e53b5c86ff0761ffefb1920f6a2814e85
ruizaul <fimbressaul@hotmail.com>
2023-03-13 11:39:13 -0700

    a ver qp

--- models/grupo.js
+++ models/grupo.js
@@ -2,6 +2,7 @@ const { Schema, model } = require('mongoose');
 
 const GruposSchema = Schema(
   {
+    _id: String,
     nombre: {
       type: String,
       required: [true, 'El nombre es obligatorio'],
