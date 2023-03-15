e6509c562751f8356a4667f4bd4fd004f4353270
ruizaul <fimbressaul@hotmail.com>
2023-03-14 23:15:41 -0700

    maso

--- middlewares/validar-datos.js
+++ middlewares/validar-datos.js
@@ -9,14 +9,13 @@ const validateLaboratorio = (req, res, next) => {
 
   Laboratorio.findOne({ laboratorio: laboratorio }, (err, laboratorio) => {
     if (err) {
-      return res.status(500).send('Error de servidor');
+      return res.status(500).json({ msg: 'Error de servidor' });
     }
 
     if (!laboratorio) {
-      return res.status(400).send('El laboratorio no existe');
+      return res.status(400).json({ msg: 'El laboratorio no existe' });
     }
 
-    req.laboratorio = laboratorio;
     next();
   });
 };
@@ -26,14 +25,13 @@ const validateCarrera = (req, res, next) => {
 
   Carrera.findOne({ carrera: carrera }, (err, carrera) => {
     if (err) {
-      return res.status(500).send('Error de servidor');
+      return res.status(500).json({ msg: 'Error de servidor' });
     }
 
     if (!carrera) {
-      return res.status(400).send('La carrera no existe');
+      return res.status(400).json({ msg: 'La carrera no existe' });
     }
 
-    req.carrera = carrera;
     next();
   });
 };
@@ -43,14 +41,13 @@ const validateMateria = (req, res, next) => {
 
   Materia.findOne({ materia: materia }, (err, materia) => {
     if (err) {
-      return res.status(500).send('Error de servidor');
+      return res.status(500).json({ msg: 'Error de servidor' });
     }
 
     if (!materia) {
-      return res.status(400).send('La materia no existe');
+      return res.status(400).json({ msg: 'La materia no existe' });
     }
 
-    req.materia = materia;
     next();
   });
 };
@@ -60,14 +57,13 @@ const validateDia = (req, res, next) => {
 
   Dia.findOne({ dia: dia }, (err, dia) => {
     if (err) {
-      return res.status(500).send('Error de servidor');
+      return res.status(500).json({ msg: 'Error de servidor' });
     }
 
     if (!dia) {
-      return res.status(400).send('El día no existe');
+      return res.status(400).json({ msg: 'El día no existe' });
     }
 
-    req.dia = dia;
     next();
   });
 };
@@ -77,14 +73,13 @@ const validateHora = (req, res, next) => {
 
   Hora.findOne({ hora: hora }, (err, hora) => {
     if (err) {
-      return res.status(500).send('Error de servidor');
+      return res.status(500).json({ msg: 'Error de servidor' });
     }
 
     if (!hora) {
-      return res.status(400).send('La hora no existe');
+      return res.status(400).json({ msg: 'La hora no existe' });
     }
 
-    req.hora = hora;
     next();
   });
 };
