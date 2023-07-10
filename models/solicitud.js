5d6347870fb248a037222d61ec8ad09116edef6b
saulprl <phasecross@hotmail.com>
2023-07-09 21:13:00 -0700

    Add timestamps to request model

--- models/solicitud.js
+++ models/solicitud.js
@@ -1,41 +1,40 @@
 const { Schema, model } = require("mongoose");
 
-const SolicitudSchema = new Schema({
-  profId: {
-    type: Schema.Types.ObjectId,
-    ref: "Usuario",
-    required: [true, "El ID del profesor es obligatorio."],
+const SolicitudSchema = new Schema(
+  {
+    profId: {
+      type: Schema.Types.ObjectId,
+      ref: "Usuario",
+      required: [true, "El ID del profesor es obligatorio."],
+    },
+    groupId: {
+      type: Schema.Types.ObjectId,
+      ref: "Grupos",
+      required: [true, "El ID del grupo es obligatorio."],
+    },
+    assignmentId: {
+      type: Schema.Types.ObjectId,
+      ref: "Practica",
+      required: [true, "El ID de la práctica es obligatorio."],
+    },
+    requestDate: {
+      type: Schema.Types.Number,
+      required: [true, "La fecha de la solicitud es obligatoria."],
+    },
+    customReagents: {
+      type: [Map],
+      required: [false],
+    },
+    customEquipment: {
+      type: [String],
+      required: [false],
+    },
+    customWaste: {
+      type: [Map],
+      required: [false],
+    },
   },
-  groupId: {
-    type: Schema.Types.ObjectId,
-    ref: "Grupos",
-    required: [true, "El ID del grupo es obligatorio."],
-  },
-  assignmentId: {
-    type: Schema.Types.ObjectId,
-    ref: "Practica",
-    required: [true, "El ID de la práctica es obligatorio."],
-  },
-  requestDate: {
-    type: Schema.Types.Number,
-    required: [true, "La fecha de la solicitud es obligatoria."],
-  },
-  createdAt: {
-    type: Schema.Types.Number,
-    default: new Date().getTime(),
-  },
-  customReagents: {
-    type: [Map],
-    required: [false],
-  },
-  customEquipment: {
-    type: [String],
-    required: [false],
-  },
-  customWaste: {
-    type: [Map],
-    required: [false],
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = model("Solicitudes", SolicitudSchema);
